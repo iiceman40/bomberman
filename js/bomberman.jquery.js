7231425279d773204b3b7b56af0e1bed6b9584ff
unknown <iice@.(none)>
2013-10-24 00:54:51 +0200

    bombs, explosions and particle test

--- js/bomberman.jquery.js
+++ js/bomberman.jquery.js
@@ -14,8 +14,9 @@ $(document).ready(function() {
 	var maxAnisotropy = 16;
 	
 	var particleMaterial;
-
+	
 	var objects = [];
+	var bombs = [];
 	var light;
 	
 	var crateTexture;
@@ -28,14 +29,29 @@ $(document).ready(function() {
 	
 	//Set the number of rows and columns for the board
 	var rows = 20;
-	var columns = 20;
+	var columns = 15;
 	
-	var litCube; 
+	var litCube;
+	
+	var particleSystem;
+	// create the particle variables
+	var particleCount = 1800;
+	var particles = new THREE.Geometry();
+	var pMaterial = new THREE.ParticleBasicMaterial({
+		color: 0xFFFFFF,
+		opacity: 0.3,
+		size: 20,
+		map: THREE.ImageUtils.loadTexture("images/particle.png"),blending: THREE.AdditiveBlending, transparent: true
+	});
 
 	createMap();
 	
 	init();
 	animate();
+	
+	var player1 = new Object();
+	player1.bombrange = 110;
+	player1.bomblimit = 1;
 	
 
 	function init() {
@@ -48,7 +64,7 @@ $(document).ready(function() {
 		
 		// Camera
 		camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 10000 );
-		camera.position.set( 0, 300, 300 );
+		camera.position.set( 0, 730, 50 );
 		camera.lookAt(scene.position);
 
 		controls = new THREE.OrbitControls( camera );
@@ -118,6 +134,7 @@ $(document).ready(function() {
 		playerObj.position.y = 25;
 		playerObj.position.z = 25;
 		playerObj.castShadow = true;
+		objects.push(playerObj);
 		
 		
 		// Enable CCD if the object moves more than 1 meter in one simulation frame
@@ -160,7 +177,7 @@ $(document).ready(function() {
 		//directions light
 		light =  new THREE.SpotLight( 0xffeeee, 1 );
 		light.shadowDarkness = 0.4;
-		light.shadowCameraVisible = true;
+		//light.shadowCameraVisible = true;
 		
 		light.shadowCameraRight    =  1300;
 		light.shadowCameraLeft     = -1300;
@@ -197,6 +214,35 @@ $(document).ready(function() {
 		document.addEventListener( 'mousedown', onDocumentMouseDown, false );
 		document.addEventListener( 'mouseup', onDocumentMouseUp, false );
 		window.addEventListener( 'resize', onWindowResize, false );
+		
+		////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		// TEST
+		
+		// now create the individual particles
+		for(var p = 0; p < particleCount; p++) {
+			// create a particle with random
+			var pX = Math.random() * 1000 -500;
+			var pY = Math.random() * 1000;
+			var pZ = Math.random() * 1000 -500;
+			var particle = new THREE.Vector3(pX, pY, pZ);
+			
+			// create a velocity vector
+			particle.velocity = new THREE.Vector3(0, -Math.random(), 0); 
+			// add it to the geometry
+			particles.vertices.push(particle);
+		}
+		
+		// create the particle system
+		particleSystem = new THREE.ParticleSystem( particles, pMaterial);
+		
+		// also update the particle system to
+		// sort the particles which enables
+		// the behaviour we want
+		particleSystem.sortParticles = true;
+		
+		// add it to the scene
+		scene.add(particleSystem);
+		////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	}
 
 	function onWindowResize() {
@@ -255,6 +301,33 @@ $(document).ready(function() {
 	//////////////////////////////////////
 	function animate() {
 		requestAnimationFrame( animate );
+		// add some rotation to the system
+  		particleSystem.rotation.y += 0.0001;
+  		
+  		var pCount = particleCount;
+		while(pCount--) {
+		    // get the particle
+		    var particle = particles.vertices[pCount];
+		
+		    // check if we need to reset
+		    if( particle.y < 0 ){
+		    	particle.y = 1000;
+		    	//particle.velocity.y = 0;
+		    }
+		
+		    // update the velocity with
+		    // a splat of randomniz
+		    //particle.velocity.y -= Math.random() * .1;
+		
+		    // and the position
+		    particle.y = particle.y + particle.velocity.y;
+		}
+		
+		// flag to the particle system
+		// that we've changed its vertices.
+		particleSystem.geometry.__dirtyVertices = true;
+
+  		
 		render();
 		update();
 	}
@@ -290,7 +363,7 @@ $(document).ready(function() {
 		if ( keyboard.pressed("S") )
 			playerObj.translateZ(  moveDistance );
 			
-		if ( keyboard.down("R") ){
+		if ( keyboard.down("space") ){
 			playerObj.material.color = new THREE.Color(0xff0000);
 			//spawnBox(crateTexture, 25, 100, 25);
 			spawnBomb();
@@ -307,27 +380,100 @@ $(document).ready(function() {
 		
 		// controls
 		controls.update();
+		
+		TWEEN.update();
 	}
 	
 	/*
 	 * FUNCTIONS
 	 */
 	function spawnBomb(){
+		// TODO check bombcount for player
 		var materialBlack = Physijs.createMaterial(
 			new THREE.MeshPhongMaterial({ color: 0x090909 }),
 			.9, // high friction
 			.0 // low restitution
 		);
 		
-		radius = 23;
+		radius = 20;
 		segments = 36;
 		rings = 36;
-		bomb = new Physijs.SphereMesh( new THREE.SphereGeometry(radius, segments, rings), materialBlack, 9999999999 );
+		var bomb = new THREE.Mesh( new THREE.SphereGeometry(radius, segments, rings), materialBlack, 9999999999 );
+		bombs.push(bomb);
+		bombIndex = jQuery.inArray(bomb, bombs);
+		console.log(bombIndex);
 		bomb.position.x = playerObj.position.x;
-		bomb.position.y = playerObj.position.y;
+		bomb.position.y = 20;
 		bomb.position.z = playerObj.position.z;
 		bomb.castShadow = true;
 		scene.add(bomb);
+		explodeBomb(bombIndex);
+		function explodeBomb(bombIndex){
+			// TODO chain reaction for bombs
+			// TODO nice effects for explosions + sounds
+			var thisBomb = bombs[bombIndex];
+			var scale = { x : 1, y: 1, z: 1 };
+			var target = { x : 1.5, y: 1.5, z: 1.2 };
+			var tween = new TWEEN.Tween(scale).to(target, 8000);
+			tween.easing(TWEEN.Easing.Elastic.InOut);
+			tween.start();
+			tween.onUpdate(function(){
+				thisBomb.scale.x = scale.x;
+				thisBomb.scale.y = scale.y;
+				thisBomb.scale.z = scale.z;
+			});
+			
+			setTimeout(function(){
+				bomb = bombs[bombIndex];
+				explosionRayCast(0,0,-1, bomb);
+				explosionRayCast(-1,0,0, bomb);
+				explosionRayCast(0,0,1, bomb);
+				explosionRayCast(1,0,0, bomb);
+				scene.remove(bomb);
+			},5000);
+		}
+	}
+	
+	function explosionRayCast(x,y,z, bomb){
+		var raycaster = new THREE.Raycaster();
+	    raycaster.ray.direction.set(x, y, z);
+	    raycaster.ray.origin.set(bomb.position.x, bomb.position.y, bomb.position.z);
+	
+	    var geometry = new THREE.Geometry();
+	
+	    intersections = raycaster.intersectObjects( objects );
+	    if ( intersections.length > 0 ) {
+	        var geometry = new THREE.Geometry();
+	
+	        // POSITION OF MESH TO SHOOT RAYS OUT OF
+	        geometry.vertices.push( bomb.position );
+	        geometry.vertices.push( intersections[0].object.position );
+	
+			var line = new THREE.Line(geometry, new THREE.LineBasicMaterial({color: 0x990000}));
+	        scene.add(line);
+	        setTimeout(function(){scene.remove(line);},1000);
+	        
+	        p1 = bomb.position.x;
+	        p2 = bomb.position.y;
+	        p3 = bomb.position.z;
+	        q1 = intersections[0].object.position.x;
+	        q2 = intersections[0].object.position.y;
+	        q3 = intersections[0].object.position.z;
+	        
+	        distance = Math.floor( Math.sqrt( Math.pow(q1-p1,2) + Math.pow(q2-p2,2) + Math.pow(q3-p3,2) ) );
+	        console.log( 'distance: ' + distance);
+	        
+	        if( distance <= player1.bombrange){
+	        	for(i=0; i<objects.length; i++){
+					if( objects[i].id == intersections[0].object.id )
+						objects.splice(i,1);
+				}
+	        	scene.remove(intersections[0].object);
+	        	// TODO check if player got blown up
+	        	// TODO spawn PowerUp
+	        	
+	        }
+	    }
 	}
 	
 	////////////////////////////////////////////
@@ -335,11 +481,10 @@ $(document).ready(function() {
 	////////////////////////////////////////////
 	function createMap(mapheight,mapwidth){
 		//Create the board, setting random squares to be obstacles
-		for (var x = 0; x < columns; x++)
-		{
+		for (var x = 0; x < rows; x++){
 			map[x] = [];
 			board[x] = [];
-			for (var y = 0; y < rows; y++) {
+			for (var y = 0; y < columns; y++) {
 				//Give each square a 25% chance of being an obstacle
 				var square = Math.floor(Math.random()*4);
 				//0 = open, 1 = obstacle
@@ -373,7 +518,7 @@ $(document).ready(function() {
 			box = new Physijs.BoxMesh(
 				new THREE.CubeGeometry( 50, 50, 50 ),
 				material,
-				999999 // mass: 0 = infinite
+				99999999 // mass: 0 = infinite
 			);
 			box.collisions = 0;
 			
@@ -387,6 +532,7 @@ $(document).ready(function() {
 			//box.addEventListener( 'collision', handleCollision );
 			//box.addEventListener( 'ready', spawnBox );
 			scene.add( box );
+			objects.push();
 			return box;
 		}
 });
